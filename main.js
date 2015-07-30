9533c177cb38d18de09f71419e1d9f19e6ba2bde
IMacMcGraw <ianmccall93@gmail.com>
2015-07-29 20:31:41 -0700

    Added .js files.

---
+++ main.js
@@ -0,0 +1,24 @@
+//region loadScript function.
+function loadScript(scriptName)
+{
+    var script = document.createElement("script");
+    script.type= 'text/javascript';
+    script.src= scriptName;
+    document.body.appendChild(script);
+}
+//endregion
+
+//region Document configuration (canvas resizing completed by the Surface class).
+document.body.style.overflow = "hidden";
+document.body.style.width = window.innerWidth;
+document.body.style.height = window.innerHeight;
+document.body.style.margin = 0;
+document.body.style.display = "block";
+//endregion
+
+
+//region Load scripts.
+loadScript("melonJS-2.1.3.js");
+loadScript("util.js");
+loadScript("gameScript.js");
+//endregion
