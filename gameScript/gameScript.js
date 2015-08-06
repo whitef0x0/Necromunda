50dbdd86a6c607ce2d93b7c9b7477d3c65e0f505
Ian <ianmccall93@gmail.com>
2015-08-06 16:27:39 -0700

    Tidied the gameScript and removed unnecessary console.log calls.

--- gameScript/gameScript.js
+++ gameScript/gameScript.js
@@ -3,14 +3,7 @@ var game = {};
 game.worldView = new eng.WorldView("guiLayer", {zIndex:1});
 
 var handleA = new eng.ControlPoint(-50, -50, game.worldView);
-
 var point = new eng.Point(0, 0, game.worldView);
-console.log(point.magnitude());
-
 var handleB = new eng.ControlPoint(50, 50, game.worldView);
-console.log(handleB.magnitude());
-
 var line = new eng.Line({x:-100, y:-100}, {x:100, y:100}, game.worldView);
-console.log(line.length());
-
 var rect = new eng.Rect({x:-50,y:-50}, {x:100,y:100}, game.worldView);
