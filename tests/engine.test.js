6098dd8ee08e26f7942cf67f734a0395fa6a7579
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-08-08 09:02:30 -0700

    added test scaffolding

--- tests/engine.test.js
+++ tests/engine.test.js
@@ -12,12 +12,8 @@ define(['engine/engine'], function(engine){
         };
     }
 
+    describe('Engine API', function () {
 
-
-
-
-    console.log(engine);
-    describe('Engine API tests', function () {
         var game = {};
         before(function () {
 
@@ -28,54 +24,134 @@ define(['engine/engine'], function(engine){
             game.worldView = new engine.worldView('guiLayer', {zIndex:1});
         });
 
+        describe('Geo tests', function () {
+            describe('Geo.Point test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
 
-        describe('Geo.Point test', function () {
-            before(function () {
-               this.x = -50;
-               this.y = 50;
-               this.radius = 2;
-               this.colour = engine.Colour.red; 
+                it('should throw an error if a non-numeric value is used for x or y', function () {
+                    (function(){
+                      new engine.Geo.Point(game.worldView, this.x, 'a', this.radius, engine.Colour.red)
+                    }).should.throw(Error('x, y of Point must be integers'));
+
+                    (function(){
+                      new engine.Geo.Point(game.worldView, 'b', this.y, this.radius, engine.Colour.red)
+                    }).should.throw(Error('x, y of Point must be integers'));
+                });
+
+                it('should throw an error if a non-numeric value is used for radius', function () {
+                    (function(){
+                        new engine.Geo.Point(game.worldView, this.x, this.y, 'nt', engine.Colour.red);
+                    }).should.throw(Error('radius of Point must be an integer'));
+                });
+
+                it('should throw an error if a negative value is used for radius', function () {
+                    (function(){
+                        new engine.Geo.Point(game.worldView, this.x, this.y, -2, engine.Colour.red);
+                    }).should.throw(Error('radius of Point must be greater than 0'));
+                });
+
+                it('should throw an error if a non-engine.WorldView value is used for viewPort', function () {
+                    (function(){
+                        new engine.Geo.Point(null, this.x, this.y, this.radius, engine.Colour.red);
+                    }).should.throw(Error('viewPort of Point must be a engine.WorldView type object'));
+                });
+
+                it('should be able to make a canvasPath without any problems', function () {
+                    var point = new engine.Geo.Point(game.worldView, this.x, this.y, this.radius, engine.Colour.red);
+                    
+                    var expectedPath = new Path2D();
+                    expectedPath.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+
+                    var actualPath = point.makeCanvasPath();
+
+                    (actualPath).should.be.eql(expectedPath);
+                });
             });
 
-            it('should throw an error if a non-numeric value is used for x or y', function () {
-                (function(){
-                  new engine.Geo.Point(game.worldView, this.x, 'a', this.radius, engine.Colour.red)
-                }).should.throw(Error('x, y of Point must be integers'));
-
-                (function(){
-                  new engine.Geo.Point(game.worldView, 'b', this.y, this.radius, engine.Colour.red)
-                }).should.throw(Error('x, y of Point must be integers'));
+            describe('Geo.PathPoint test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
             });
 
-            it('should throw an error if a non-numeric value is used for radius', function () {
-                (function(){
-                    new engine.Geo.Point(game.worldView, this.x, this.y, 'nt', engine.Colour.red);
-                }).should.throw(Error('radius of Point must be an integer'));
+            describe('Geo.Path test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
             });
 
-            it('should throw an error if a negative value is used for radius', function () {
-                (function(){
-                    new engine.Geo.Point(game.worldView, this.x, this.y, -2, engine.Colour.red);
-                }).should.throw(Error('radius of Point must be greater than 0'));
+            describe('Geo.PathPoint test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
             });
 
-            it('should throw an error if a non-engine.WorldView value is used for viewPort', function () {
-                (function(){
-                    new engine.Geo.Point(null, this.x, this.y, this.radius, engine.Colour.red);
-                }).should.throw(Error('viewPort of Point must be a engine.WorldView type object'));
+            describe('Geo.Vector test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
             });
 
-            it('should be able to make a canvasPath without any problems', function () {
-                var point = new engine.Geo.Point(game.worldView, this.x, this.y, this.radius, engine.Colour.red);
-                
-                var expectedPath = new Path2D();
-                expectedPath.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+            describe('Geo.Line test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
+            });
 
-                var actualPath = point.makeCanvasPath();
+            describe('Geo.PathLine test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
+            });
 
-                console.log(actualPath);
-                console.log(expectedPath);
-                (actualPath).should.be.eql(expectedPath);
+            describe('Geo.Combatant test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
+            });
+
+            describe('Geo.Rect test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
+            });
+
+            describe('Geo.Path test', function () {
+                before(function () {
+                   this.x = -50;
+                   this.y = 50;
+                   this.radius = 2;
+                   this.colour = engine.Colour.red; 
+                });
             });
         });
     });
