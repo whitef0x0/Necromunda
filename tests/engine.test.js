e24ecb4ed5c468d2464491b7dce94c76e2a46b65
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-08-08 08:58:46 -0700

    got Point unit tests to pass

--- tests/engine.test.js
+++ tests/engine.test.js
@@ -13,20 +13,23 @@ define(['engine/engine'], function(engine){
     }
 
 
+
+
+
     console.log(engine);
     describe('Engine API tests', function () {
+        var game = {};
         before(function () {
 
             /* if (window.__html__) {
                 document.body.innerHTML = window.__html__['index.html'];
             }*/
 
-            this.game = {};
-            this.game.worldView = new engine.worldView('guiLayer', {zIndex:1});
+            game.worldView = new engine.worldView('guiLayer', {zIndex:1});
         });
 
 
-        describe('Point Geometric Figure', function () {
+        describe('Geo.Point test', function () {
             before(function () {
                this.x = -50;
                this.y = 50;
@@ -35,28 +38,45 @@ define(['engine/engine'], function(engine){
             });
 
             it('should throw an error if a non-numeric value is used for x or y', function () {
-                var point = new engine.Geo.Point(this.game.worldView, this.x, 'a', this.radius, engine.Colour.red).should.throw(Error, 'x, y of Point must be integers');
-                var point = new engine.Geo.Point(this.game.worldView, 'a', this.y, this.radius, engine.Colour.red).should.throw(Error, 'x, y of Point must be integers');
+                (function(){
+                  new engine.Geo.Point(game.worldView, this.x, 'a', this.radius, engine.Colour.red)
+                }).should.throw(Error('x, y of Point must be integers'));
+
+                (function(){
+                  new engine.Geo.Point(game.worldView, 'b', this.y, this.radius, engine.Colour.red)
+                }).should.throw(Error('x, y of Point must be integers'));
             });
 
             it('should throw an error if a non-numeric value is used for radius', function () {
-                var point = new engine.Geo.Point(this.game.worldView, this.x, this.y, 'nt', engine.Colour.red).should.throw(Error, 'radius of Point must be an integer');
+                (function(){
+                    new engine.Geo.Point(game.worldView, this.x, this.y, 'nt', engine.Colour.red);
+                }).should.throw(Error('radius of Point must be an integer'));
             });
 
             it('should throw an error if a negative value is used for radius', function () {
-                var point = new engine.Geo.Point(this.game.worldView, this.x, this.y, -2, engine.Colour.red).should.throw(Error, 'radius of Point must be greater than 0');
+                (function(){
+                    new engine.Geo.Point(game.worldView, this.x, this.y, -2, engine.Colour.red);
+                }).should.throw(Error('radius of Point must be greater than 0'));
             });
 
             it('should throw an error if a non-engine.WorldView value is used for viewPort', function () {
-                var point = new engine.Geo.Point(null, this.x, this.y, this.radius, engine.Colour.red).should.throw(Error, 'viewPort of Point must be a engine.WorldView type object');
+                (function(){
+                    new engine.Geo.Point(null, this.x, this.y, this.radius, engine.Colour.red);
+                }).should.throw(Error('viewPort of Point must be a engine.WorldView type object'));
             });
 
             it('should be able to make a canvasPath without any problems', function () {
                 var point = new engine.Geo.Point(game.worldView, this.x, this.y, this.radius, engine.Colour.red);
-                var arcPath = new Path2D().arc(this.x, this.y, this.radius, 0, this.radius*Math.PI);
-                (point.makeCanvasPath()).should.be.equal(arcPath);
-            });
+                
+                var expectedPath = new Path2D();
+                expectedPath.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
 
+                var actualPath = point.makeCanvasPath();
+
+                console.log(actualPath);
+                console.log(expectedPath);
+                (actualPath).should.be.eql(expectedPath);
+            });
         });
     });
 
