221a11a17ec58d123508925682f2598d6e27bb96
David Baldwynn <whitef0x0@users.noreply.github.com>
2015-08-08 10:15:27 -0700

    readded Number prototypes

--- engine/engine.js
+++ engine/engine.js
@@ -1,3 +1,12 @@
+//region Math and number related functions and methods.
+// Number.prototype.toHex = function () {
+//     return "0123456789ABCDEF".charAt((this - (this % 16)) >> 4) + "0123456789ABCDEF".charAt(this % 16);
+// };
+Number.prototype.clamp = function (low, high) {
+   return this < low ? low : this > high ? high : +this;
+}
+//end region
+
 define(function (require) {
     
     var eng = {};
